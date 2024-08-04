0d159a6fc71fc4074231dcd05365ef1a9a17fe55
Pablo Aguirre <pablofabricio.aguirreguaman@studenti.unimi.it>
2024-08-04 16:22:16 +0200

    Initial Commit

---
+++ ARForniture/ARFornitureApp.swift
@@ -0,0 +1,17 @@
+//
+//  ARFornitureApp.swift
+//  ARForniture
+//
+//  Created by Pablo Aguirre on 04/08/24.
+//
+
+import SwiftUI
+
+@main
+struct ARFornitureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
